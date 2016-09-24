515d4c18885ff57538de3ea5e3a6f3a8ca0f074f
ZRazor <exp.razor@gmail.com>
2016-09-24 11:42:24 +1000

    Add profile controller

---
+++ rostelSources/Controllers/Profile/MKRProfileViewController.h
@@ -0,0 +1,13 @@
+//
+//  MKRProfileViewController.h
+//  rostel
+//
+//  Created by Anton Zlotnikov on 24.09.16.
+//  Copyright © 2016 mayak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MKRProfileViewController : UITableViewController
+
+@end
---
+++ rostelSources/Controllers/Profile/Views/MKRProfileAchievementCollectionViewCell.m
@@ -0,0 +1,13 @@
+//
+//  MKRProfileAchievementCollectionViewCell.m
+//  rostel
+//
+//  Created by Anton Zlotnikov on 24.09.16.
+//  Copyright © 2016 mayak. All rights reserved.
+//
+
+#import "MKRProfileAchievementCollectionViewCell.h"
+
+@implementation MKRProfileAchievementCollectionViewCell
+
+@end
