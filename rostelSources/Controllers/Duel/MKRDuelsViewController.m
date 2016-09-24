7db7201281e82b994e2cb5cca3a00af439f36de0
Zinov Mikhail <zinov.m.v@gmail.com>
2016-09-24 21:05:12 +1000

    Правил дуэли

--- rostelSources/Controllers/Duel/MKRDuelsViewController.m
+++ rostelSources/Controllers/Duel/MKRDuelsViewController.m
@@ -27,6 +27,9 @@ static NSString *const kMKRDuelCellIdentifier = @"duelCell";
     MKRDuelsListPresenter *presenter;
     BOOL isLoadingDuels;
     UIRefreshControl *refreshControl;
+    IBOutlet UILabel *loseCountLabel;
+    IBOutlet UILabel *drawCountLabel;
+    IBOutlet UILabel *winCountLabel;
 }
 
 - (void)viewDidLoad {
@@ -41,6 +44,7 @@ static NSString *const kMKRDuelCellIdentifier = @"duelCell";
     presenter = [[MKRDuelsListPresenter alloc] init];
     [presenter setDelegate:self];
     [presenter updateDuels];
+    [self.tableView setRowHeight:UITableViewAutomaticDimension];
     // Uncomment the following line to preserve selection between presentations.
      self.clearsSelectionOnViewWillAppear = NO;
     
@@ -174,59 +178,8 @@ static NSString *const kMKRDuelCellIdentifier = @"duelCell";
     [hud.bezelView setStyle:MBProgressHUDBackgroundStyleSolidColor];
 }
 
-
-/*
-- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:<#@"reuseIdentifier"#> forIndexPath:indexPath];
-    
-    // Configure the cell...
-    
-    return cell;
+- (CGFloat)tableView:(UITableView *)tableView estimatedHeightForRowAtIndexPath:(NSIndexPath *)indexPath {
+    return 70;
 }
-*/
-
-/*
-// Override to support conditional editing of the table view.
-- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
-    // Return NO if you do not want the specified item to be editable.
-    return YES;
-}
-*/
-
-/*
-// Override to support editing the table view.
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
-    if (editingStyle == UITableViewCellEditingStyleDelete) {
-        // Delete the row from the data source
-        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-    }   
-}
-*/
-
-/*
-// Override to support rearranging the table view.
-- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
-}
-*/
-
-/*
-// Override to support conditional rearranging of the table view.
-- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
-    // Return NO if you do not want the item to be re-orderable.
-    return YES;
-}
-*/
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
